cba5b5f85865957af2798e3af77e47222da634b4
Moringa <you@example.com>
2021-06-24 18:40:05 +0300

    Update changes

--- src/main/java/dao/DepartmentDao.java
+++ src/main/java/dao/DepartmentDao.java
@@ -7,17 +7,17 @@ import java.util.List;
 
 public interface DepartmentDao {
     //CREATE
-    void add(Employee employee);
-    void addDepartmentUser(Employee employee, Departments department);
+    void add(Departments departments);
+    void addDepartmentEmployee(Departments departments, Employee employee);
 
     //READ
-    List<Employee> getAll();
-    List<Employee>getAllDepartmentUser(int departmentId);
+    List<Departments> getAll();
+    List<Employee>getAllDepartmentEmployee(int departmentId);
 
-    Employee findById(int id);
+    Departments findById(int id);
 
     //UPDATE
-    void update(Employee employee, int id,String name,String position,String role,int departmentId);
+    void update(Departments departments, int id,String name, int num_members);
 
     //DELETE
     void clearAll();
--- src/main/java/dao/Sql2oDepartmentDao.java
+++ src/main/java/dao/Sql2oDepartmentDao.java
@@ -2,38 +2,57 @@ package dao;
 
 import models.Departments;
 import models.Employee;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+import org.sql2o.Connection;
 
 import java.util.List;
 
 public class Sql2oDepartmentDao implements DepartmentDao{
+    private Sql2o sql2o;
+
+    public Sql2oDepartmentDao(Sql2o sql2o){
+        this.sql2o=sql2o;
+    }
 
     @Override
-    public void add(Employee employee) {
+    public void add(Departments departments) {
+        String sql = "INSERT INTO departments (name, description) VALUES (:name, :description)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(departments)
+                    .executeUpdate()
+                    .getKey();
+            Departments.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
 
     }
 
     @Override
-    public void addDepartmentUser(Employee employee, Departments department) {
+    public void addDepartmentEmployee(Departments departments, Employee employee) {
 
     }
 
     @Override
-    public List<Employee> getAll() {
+    public List<Departments> getAll() {
+        return null;
+    }
+
+
+    @Override
+    public List<Employee> getAllDepartmentEmployee(int departmentId) {
         return null;
     }
 
     @Override
-    public List<Employee> getAllDepartmentUser(int departmentId) {
+    public Departments findById(int id) {
         return null;
     }
 
     @Override
-    public Employee findById(int id) {
-        return null;
-    }
-
-    @Override
-    public void update(Employee employee, int id, String name, String position, String role, int departmentId) {
+    public void update(Departments departments, int id, String name, int num_members) {
 
     }
 
