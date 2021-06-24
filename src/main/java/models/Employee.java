bd0651a4d1d8a9bf62942b2cbd918c5556b26521
Moringa <you@example.com>
2021-06-24 17:22:18 +0300

    Added constructors,getters and setter

--- src/main/java/models/Employee.java
+++ src/main/java/models/Employee.java
@@ -9,7 +9,49 @@ public class Employee {
 
     public Employee(int id,String name,String position,String role,int DepartmentId){
         this.id=id;
+        this.name=name;
         this.position=position;
         this.role=role;
+        this.DepartmentId=DepartmentId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getDepartmentId() {
+        return DepartmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        DepartmentId = departmentId;
     }
 }
