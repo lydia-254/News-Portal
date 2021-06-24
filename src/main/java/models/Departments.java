6b0e87450de8ea1b2405da450b44121ffd3af360
Moringa <you@example.com>
2021-06-24 17:11:29 +0300

    Created constructor in Departments class

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,4 +1,13 @@
 package models;
 
 public class Departments {
+    private int id;
+    private String name;
+    private int num_members;
+
+    public Departments(int id,String name ,int num_members){
+        this.id=id;
+        this.name=name;
+        this.num_members=num_members;
+    }
 }
