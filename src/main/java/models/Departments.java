878554002b64d11b3678f5a835ea455628d6a582
Moringa <you@example.com>
2021-06-24 17:20:38 +0300

    Added getters and setters in Departments calss

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -10,4 +10,28 @@ public class Departments {
         this.name=name;
         this.num_members=num_members;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNum_members() {
+        return num_members;
+    }
+
+    public void setNum_members(int num_members) {
+        this.num_members = num_members;
+    }
 }
--- src/main/java/models/Employee.java
+++ src/main/java/models/Employee.java
@@ -1,4 +1,15 @@
 package models;
 
 public class Employee {
+    private int id;
+    private String name;
+    private String position;
+    private String role;
+    private int DepartmentId;
+
+    public Employee(int id,String name,String position,String role,int DepartmentId){
+        this.id=id;
+        this.position=position;
+        this.role=role;
+    }
 }
