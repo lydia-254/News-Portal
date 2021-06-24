65d60fb67ac157423e150a750de912475138d45f
Moringa <you@example.com>
2021-06-24 17:58:24 +0300

    Add updates on Sql2oNewsDao

--- src/main/java/dao/Sql2oNewsDao.java
+++ src/main/java/dao/Sql2oNewsDao.java
@@ -1,4 +1,43 @@
 package dao;
 
-public class Sql2oNewsDao {
+import models.Departments;
+import models.Employee;
+
+import java.util.List;
+
+public class Sql2oNewsDao implements NewsDao{
+    @Override
+    public void add(Employee employee) {
+
+    }
+
+    @Override
+    public void addDepartmentUser(Employee employee, Departments department) {
+
+    }
+
+    @Override
+    public List<Employee> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Employee> getAllDepartmentUser(int departmentId) {
+        return null;
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(Employee employee, int id, String name, String position, String role, int departmentId) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
