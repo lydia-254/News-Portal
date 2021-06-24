75262ed4873ef075a112cfe1f165158f29eac031
Moringa <you@example.com>
2021-06-24 17:53:57 +0300

    Add updates on Sql2oDepartmentDao

--- src/main/java/dao/Sql2oDepartmentDao.java
+++ src/main/java/dao/Sql2oDepartmentDao.java
@@ -1,4 +1,44 @@
 package dao;
 
-public class Sql2oDepartmentDao {
+import models.Departments;
+import models.Employee;
+
+import java.util.List;
+
+public class Sql2oDepartmentDao implements DepartmentDao{
+
+    @Override
+    public void add(Employee employee) {
+
+    }
+
+    @Override
+    public void addDepartmentUser(Employee employee, Departments department) {
+
+    }
+
+    @Override
+    public List<Employee> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Employee> getAllDepartmentUser(int departmentId) {
+        return null;
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(Employee employee, int id, String name, String position, String role, int departmentId) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
