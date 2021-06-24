ee12c6acecf105d2703d39217b0bee6887616809
Moringa <you@example.com>
2021-06-24 17:39:33 +0300

    Update NewsDao

--- src/main/java/dao/EmployeeDao.java
+++ src/main/java/dao/EmployeeDao.java
@@ -1,4 +1,24 @@
 package dao;
 
+import models.Departments;
+import models.Employee;
+
+import java.util.List;
+
 public interface EmployeeDao {
+    //CREATE
+    void add(Employee employee);
+    void addDepartmentUser(Employee employee, Departments department);
+
+    //READ
+    List<Employee> getAll();
+    List<Employee>getAllDepartmentUser(int departmentId);
+
+    Employee findById(int id);
+
+    //UPDATE
+    void update(Employee employee, int id,String name,String position,String role,int departmentId);
+
+    //DELETE
+    void clearAll();
 }
