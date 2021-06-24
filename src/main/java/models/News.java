96b8d3475267f99b4fbd69e599859ca4f4610dff
Moringa <you@example.com>
2021-06-24 17:31:10 +0300

    Added constructors and setters and getters

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,47 @@
 package models;
 
 public class News {
+    private int id;
+    private String content;
+    private int userId;
+    private int DepartmentId;
+
+    public News(int id,String content,int userId,int DepartmentId){
+        this.id=id;
+        this.content=content;
+        this.userId=userId;
+        this.DepartmentId=DepartmentId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getDepartmentId() {
+        return DepartmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        DepartmentId = departmentId;
+    }
 }
