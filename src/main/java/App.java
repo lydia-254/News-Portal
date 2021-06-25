b4c15b4d062d4c04d4c20e3eeb57ab509457776b
Moringa <you@example.com>
2021-06-25 13:30:59 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,8 +2,12 @@ import com.google.gson.Gson;
 import dao.Sql2oDepartmentDao;
 import dao.Sql2oEmployeeDao;
 import dao.Sql2oNewsDao;
+import models.Employee;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+
+import static spark.Spark.*;
+
 
 public class App {
     public static void main(String[] args) {
@@ -11,12 +15,20 @@ public class App {
         Sql2oNewsDao newsDao;
         Sql2oEmployeeDao employeeDao;
         Connection conn;
-        Gson gson =new Gson();
-        String connectionString="jdbc:postgresql://localhost:5432/news_portal";
-        Sql2o sql2o=new Sql2o(connectionString,"postgres","Access");
+        Gson gson = new Gson();
+        String connectionString = "jdbc:postgresql://localhost:5432/news_portal";
+        Sql2o sql2o = new Sql2o(connectionString, "postgres", "Access");
         departmentDao = new Sql2oDepartmentDao(sql2o);
         newsDao = new Sql2oNewsDao(sql2o);
         employeeDao = new Sql2oEmployeeDao(sql2o);
         conn = sql2o.open();
+
+        post("/employee/new", "application/json", (req, res) -> {
+            Employee employee = gson.fromJson(req.body(), Employee.class);
+            employeeDao.add(employee);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(employeeDao.getAll());
+        });
     }
 }
