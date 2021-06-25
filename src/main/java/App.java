03f7e5caba402dc94476c7c0f95a0b4c581f42c4
Moringa <you@example.com>
2021-06-25 13:21:36 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,22 @@
-public class App {
+import com.google.gson.Gson;
+import dao.Sql2oDepartmentDao;
+import dao.Sql2oEmployeeDao;
+import dao.Sql2oNewsDao;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
+public class App {
+    public static void main(String[] args) {
+        Sql2oDepartmentDao departmentDao;
+        Sql2oNewsDao newsDao;
+        Sql2oEmployeeDao employeeDao;
+        Connection conn;
+        Gson gson =new Gson();
+        String connectionString="jdbc:postgresql://localhost:5432/news_portal";
+        Sql2o sql2o=new Sql2o(connectionString,"postgres","Access");
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        newsDao = new Sql2oNewsDao(sql2o);
+        employeeDao = new Sql2oEmployeeDao(sql2o);
+        conn = sql2o.open();
+    }
 }
